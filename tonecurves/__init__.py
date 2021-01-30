bbfd9e0c5b4ed168d12fc665ceffc9bfdb8c9762
jandren <andren.jakob@gmail.com>
2021-01-30 15:46:53 +0100

    Remove unused functionallity

--- tonecurves/__init__.py
+++ tonecurves/__init__.py
@@ -1,6 +1,4 @@
 from .basecurve import BaseCurve
 from .filmic import Filmic, FilmicSettings
 from .loglogistic import LogLogistic, LogLogisticSettings
-from .weibull import Weibull, WeibullSettings
-from .sigmoid import Sigmoid, SigmoidSettings
 from .scene_light import *
