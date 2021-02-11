0f848db4b5ea9d20b6e4435d1db4609b41740f2f
jandren <andren.jakob@gmail.com>
2021-02-11 20:17:06 +0100

    Add additional view modes for the graphs

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,11 @@ import tonecurves
 ### Sidebar stuff
 st.sidebar.header("Scene Settings")
 view_range = st.sidebar.slider('View Range', min_value=-16.0, max_value=16.0,value=[-8.0, 5.0])
-view_mode = st.sidebar.selectbox('View Scaling', ["EV (log2)", "Linear"])
+view_mode = st.sidebar.selectbox('View Scaling', ["EV (log2)", "Linear", "Log2Log2"])
+if not view_mode == "Log2Log2":
+    view_dydx_mode = st.sidebar.selectbox('Slope Base', ["Linear", "EV (log2)"])
+else:
+    view_dydx_mode = "EV (log2)"
 view_resolution = st.sidebar.slider('View Resolution', min_value=100, max_value=10000,value=400)
 
 st.sidebar.header("Display Settings")
@@ -59,6 +63,11 @@ value_plot = pd.DataFrame(index=view_x_axis)
 slope_plot = pd.DataFrame(index=view_x_axis)
 
 intensity_x_axis = tonecurves.scene_light_linear(view_range, view_mode, view_resolution)
+if view_dydx_mode == "Linear":
+    dydx_x_axis = intensity_x_axis
+else:
+    dydx_x_axis = np.log2(intensity_x_axis)
+log_epsilon = pow(2.0, -16)
 
 @st.cache
 def derivative(x, y):
@@ -70,14 +79,18 @@ def derivative(x, y):
 # Add Loglogistic to the DataFrame
 loglogistic = tonecurves.LogLogistic()
 loglogistic_value = loglogistic.apply(intensity_x_axis, loglogistic_settings)
-loglogistic_slope = derivative(intensity_x_axis, loglogistic_value)
+if view_mode == "Log2Log2":
+    loglogistic_value = np.log2(loglogistic_value + log_epsilon)
+loglogistic_slope = derivative(dydx_x_axis, loglogistic_value)
 value_plot["LogLogistic"] = loglogistic_value
 slope_plot["LogLogistic"] = loglogistic_slope
 
 # Add Filmic to the DataFrame
 filmic = tonecurves.Filmic()
 filmic_value = filmic.apply(intensity_x_axis, filmic_settings)
-filmic_slope = derivative(intensity_x_axis, filmic_value)
+if view_mode == "Log2Log2":
+    filmic_value = np.log2(filmic_value + log_epsilon)
+filmic_slope = derivative(dydx_x_axis, filmic_value)
 value_plot["Filmic"] = filmic_value
 slope_plot["Filmic"] = filmic_slope
 
@@ -104,7 +117,9 @@ def get_base_curve(mode, intensity):
 
 if show_base_curve:
     base_display, base_slope = get_base_curve(view_mode, intensity_x_axis)
-    base_slope = derivative(intensity_x_axis, base_display)
+    if view_mode == "Log2Log2":
+        base_display = np.log2(base_display + log_epsilon)
+    base_slope = derivative(dydx_x_axis, base_display)
     value_plot["Average Base Curve"] = base_display
     slope_plot["Average Base Curve"] = base_slope
 
@@ -114,13 +129,17 @@ if show_aces_srgb or show_aces_hlg:
 
 if show_aces_srgb:
     aces_srgb_display = aces.apply_srgb(intensity_x_axis)
-    aces_srgb_slope = derivative(intensity_x_axis, aces_srgb_display)
+    if view_mode == "Log2Log2":
+        aces_srgb_display = np.log2(aces_srgb_display + log_epsilon)
+    aces_srgb_slope = derivative(dydx_x_axis, aces_srgb_display)
     value_plot["ACES sRGB"] = aces_srgb_display
     slope_plot["ACES sRGB"] = aces_srgb_slope
 
 if show_aces_hlg:
     aces_hlg_display = aces.apply_hlg(intensity_x_axis)
-    aces_hlg_slope = derivative(intensity_x_axis, aces_hlg_display)
+    if view_mode == "Log2Log2":
+        aces_hlg_display = np.log2(aces_hlg_display + log_epsilon)
+    aces_hlg_slope = derivative(dydx_x_axis, aces_hlg_display)
     value_plot["ACES HLG"] = aces_hlg_display
     slope_plot["ACES HLG"] = aces_hlg_slope
 
@@ -132,7 +151,9 @@ def get_blender_curve(curve_name):
 
 if not show_blender_filmic == "None":
     blender_display = get_blender_curve(show_blender_filmic)
-    blender_slope = derivative(intensity_x_axis, blender_display)
+    if view_mode == "Log2Log2":
+        blender_display = np.log2(blender_display + log_epsilon)
+    blender_slope = derivative(dydx_x_axis, blender_display)
     value_plot[show_blender_filmic] = blender_display
     slope_plot[show_blender_filmic] = blender_slope
 
@@ -145,7 +166,8 @@ I hope some you will find it interesting as well!""")
 st.header("Usage")
 st.write("""The view range on the left is for the x-axis luminance range, which is defined in EV relative to
 middle grey, fixed at 0.1845. The view scaling is only used for scaling the plots. However all calculations are always
-done on the linear light intensity.
+done on the linear light intensity. The basis for calculation of the the derivative can be picked as linear or EV
+indenpendetly of the view scaling. Log2Log2 forces log2 basis for the derivative.
 
 Display settings are the same as the display tab in the filmic module with 1.0=100%.
 The defualt values of 0 and 1 equals the range of a normal computer screen. A HLG HDR screen will typically have a target white luminance = 12.
