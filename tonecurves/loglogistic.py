0c23117c6c38790a2bf918125f3b7c140fd2190e
jandren <andren.jakob@gmail.com>
2021-03-27 08:40:54 +0100

    Orthgonal skew and contrast

--- tonecurves/loglogistic.py
+++ tonecurves/loglogistic.py
@@ -8,20 +8,51 @@ class LogLogistic(object):
         self.display_white = 1.0
         self.contrast = 1.65
         self.skew = 0.0
+    
+    def find_film_power(self):
+        # Actual wanted slope should be the same as for without skew
+        # and with constant display targets
+        self.paper_power = 1.0
+        self.film_power = self.contrast
+        self.magnitude = 1.0
+        self.fog = 0.0
+        self.paper_e = pow(self.scene_grey, self.film_power) * ((self.magnitude / self.display_grey) - 1.0)
+        target_slope = self.__calc_slope_at(self.scene_grey)
+
+        # Add skew
+        self.paper_power = pow(5.0, -self.skew)
+
+        # Slope at low film power
+        self.film_power = 1.0
+        self.__settings()
+        self.fog = 0.0
+        slope_1 = self.__calc_slope_at(self.scene_grey)
+
+        # Figure out where it fulfills the target slope
+        # (linear when assuming display_black=0.0)
+        self.film_power = target_slope / slope_1
+
+        # Redo settings such that black target can be taken into consideration
+        self.__settings()
+        return self.film_power
+
+    def __calc_slope_at(self, point):
+        dt = 0.00000001
+        return (self.__evaluate(self.scene_grey + dt) - self.__evaluate(self.scene_grey - dt)) / 2 / dt
 
     def __settings(self):
-        self.skew_power = pow(5.0, -self.skew)
-        self.power = pow(self.contrast, 1.0 / self.skew_power)
-
         self.magnitude = self.display_white
-        T = pow(self.display_white / self.display_grey, 1.0 / self.skew_power) - 1.0
+        T = pow(self.display_white / self.display_grey, 1.0 / self.paper_power) - 1.0
         if self.display_black > 0.0:
-            z = pow(self.display_white / self.display_black, 1.0 / self.skew_power) - 1.0
-            self.fog = self.scene_grey * pow(T, 1.0 / self.power) / (pow(z, 1.0 / self.power) - pow(T, 1.0 / self.power))
+            z = pow(self.display_white / self.display_black, 1.0 / self.paper_power) - 1.0
+            self.fog = self.scene_grey * pow(T, 1.0 / self.film_power) / (pow(z, 1.0 / self.film_power) - pow(T, 1.0 / self.film_power))
         else:
             self.fog = 0.0
-        self.paper_e = pow(self.fog + self.scene_grey, self.power) * T        
+        self.paper_e = pow(self.fog + self.scene_grey, self.film_power) * T
+
+    def __evaluate(self, x):
+        return self.magnitude * pow(1.0 + self.paper_e * pow(self.fog + x, -self.film_power), -self.paper_power)
 
     def apply(self, x):
-        self.__settings()
-        return self.magnitude * pow(1.0 + self.paper_e * pow(self.fog + x, -self.power), -self.skew_power)
+        self.find_film_power()
+        return self.__evaluate(x)
